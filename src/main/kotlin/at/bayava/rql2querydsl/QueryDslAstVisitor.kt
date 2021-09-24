29c1b82cc3e7c7a4b05d85ab5dda66e1ebed277d
phil-ba <phi.ba@yandex.com>
2021-09-25 00:11:19 +0200

    Refactored logic into seperate methods. Some code cleanup

--- src/main/kotlin/at/bayava/rql2querydsl/QueryDslAstVisitor.kt
+++ src/main/kotlin/at/bayava/rql2querydsl/QueryDslAstVisitor.kt
@@ -11,14 +11,15 @@ import net.jazdw.rql.parser.ASTNode
 import net.jazdw.rql.parser.SimpleASTVisitor
 
 private val logger = KotlinLogging.logger {}
+
 class QueryDslAstVisitor(private val clazz: Class<*>) : SimpleASTVisitor<Predicate> {
 
-    val operatorsBySymbol: Map<String, RqlOperator> =
-        DefaultLogicalOperators.values()
-            .map { it.operator }
-            .plus(DefaultComparisonOperators.values().map { it.operator })
-            .flatMap { it.symbol.map { symbol -> symbol to it } }
-            .toMap()
+    private val operatorsBySymbol: Map<String, RqlOperator> = DefaultLogicalOperators.values()
+        .map { it.operator }
+        .plus(DefaultComparisonOperators.values()
+            .map { it.operator })
+        .flatMap { it.symbol.map { symbol -> symbol to it } }
+        .toMap()
 
     override fun visit(node: ASTNode): Predicate {
         logger.debug { "Node[${node}]" }
@@ -26,48 +27,60 @@ class QueryDslAstVisitor(private val clazz: Class<*>) : SimpleASTVisitor<Predica
         val arguments = node.arguments
         logger.debug { "Operator[${operator}]" }
         return when (operator) {
-            is LogicalOperator -> {
-                if (arguments.any { it is ASTNode == false }) throw IllegalArgumentException("Operator[${operator}] can only contain other operators!")
-                val nodeArguments = arguments as List<ASTNode>
-                val args = nodeArguments.map { it.accept<Predicate, Void>(this) }
-                return when (operator.qOperator) {
-                    Ops.AND -> ExpressionUtils.allOf(args)!!
-                    Ops.OR -> ExpressionUtils.anyOf(args)!!
-                    else -> throw IllegalArgumentException("Logical op[${operator.qOperator}] not implemented!")
-                }
-            }
-            is ComparisonOperator -> {
-                if (arguments.any { it is ASTNode }) throw IllegalArgumentException("Operator[${operator}] can't contain other operators!")
-                val selector = arguments[0] as String
-                val opArguments = arguments.drop(1) as List<Any>
-                val path: PathBuilder<Any> = PathBuilder(
-                    clazz,
-                    "root"
-                ).get(selector)
+            is LogicalOperator -> handleLogicalOperator(
+                arguments,
+                operator
+            )
+            is ComparisonOperator -> handleBooleanOperator(
+                arguments,
+                operator
+            )
 
-//                val field = (clazz::getDeclaredField)(selector)
-
-
-                val constant = if (operator.multiValue)
-                    Expressions.list(
-                        *opArguments.map {
-                            Expressions.constant(it)
-                        }.toTypedArray()
-                    )
-                else Expressions.constant(
-                    opArguments
-//                        convert(
-//                            node,
-//                            field.type
-//                        )
-                )
-                return Expressions.predicate(
-                    operator.qOperator,
-                    path,
-                    constant
-                )
-            }
             else -> throw IllegalArgumentException("Operator[${operator::class}] type not implemented!")
+        }
+    }
+
+    private fun handleBooleanOperator(arguments: MutableList<Any>,
+        operator: RqlOperator
+    ): Predicate {
+        if (arguments.any { it is ASTNode }) throw IllegalArgumentException("Operator[${operator}] can't contain other operators!")
+        val selector = arguments[0] as String
+        val opArguments = arguments.drop(1)
+        val path: PathBuilder<Any> = PathBuilder(
+            clazz,
+            "root"
+        ).get(selector)
+
+        //                val field = (clazz::getDeclaredField)(selector)
+
+        val constant = if (operator.multiValue) Expressions.list(*opArguments.map {
+            Expressions.constant(it)
+        }
+            .toTypedArray())
+        else Expressions.constant(
+            opArguments
+            //                        convert(
+            //                            node,
+            //                            field.type
+            //                        )
+        )
+        return Expressions.predicate(
+            operator.qOperator,
+            path,
+            constant
+        )
+    }
+
+    private fun handleLogicalOperator(arguments: MutableList<Any>,
+        operator: RqlOperator
+    ): Predicate {
+        if (arguments.any { it is ASTNode == false }) throw IllegalArgumentException("Operator[${operator}] can only contain other operators!")
+        val nodeArguments = arguments as List<ASTNode>
+        val args = nodeArguments.map { it.accept<Predicate, Void>(this) }
+        return when (operator.qOperator) {
+            Ops.AND -> ExpressionUtils.allOf(args)!!
+            Ops.OR -> ExpressionUtils.anyOf(args)!!
+            else -> throw IllegalArgumentException("Logical op[${operator.qOperator}] not implemented!")
         }
     }
 
